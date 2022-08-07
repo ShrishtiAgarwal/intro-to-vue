22429b455948549b2b7dbfec4c146b6f6ee60fd2
ShrishtiAgarwal <shrishtiagarwal21@gmail.com>
2022-08-08 01:29:09 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import TodoApp from './components/TodoApp.vue';
+</script>
+
+<template>
+  <header>
+     <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+
+    <div class="wrapper">
+      <TodoApp msg="You did it!" />
+    </div> 
+  </header>
+
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+</template>
+
